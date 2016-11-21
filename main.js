38a7e0f0b74836244b7f430cdc0b3076dc9d0295
mrquan122 <503136506@qq.com>
2016-11-21 17:13:41 +0800

    mainjs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  require.config({
             paths : {
-                "jquery" : ["http://libs.baidu.com/jquery/2.0.3/jquery","jquery/jquery-1.7.1.min.js"],
+                "jquery" : "jquery/jquery-1.7.1.min.js",
                 "zoom":"js/zoom" ,
                 "list_delegate":"js/list_delegate",
                 "navigate":"js/navigate"
